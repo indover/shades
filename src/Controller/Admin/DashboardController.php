557bf071670efac1c174203dc627b1b9ddabbd6a
indover <innate@ukr.net>
2023-07-28 15:42:32 -0700

    add files

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Gallery;
 use App\Entity\Service;
 use App\Entity\ServiceItem;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
@@ -16,23 +17,8 @@ class DashboardController extends AbstractDashboardController
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-//        return parent::index();
-//        return $this->render('admin/index.html.twig');
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
          $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
          return $this->redirect($adminUrlGenerator->setController(ServiceCrudController::class)->generateUrl());
-
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-        // return $this->render('some/path/my-dashboard.html.twig');
     }
 
     public function configureDashboard(): Dashboard
@@ -44,7 +30,8 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('Service', 'fas fa-list', Service::class);
+        yield MenuItem::linkToCrud('Service', 'fa-regular fa-envelope-open', Service::class);
         yield MenuItem::linkToCrud('Item', 'fas fa-list', ServiceItem::class);
+        yield MenuItem::linkToCrud('Gallery', 'fa-regular fa-images', Gallery::class);
     }
 }
--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Service\GalleryService;
 use App\Service\ServiceItemsService;
 use App\Service\ServiceService;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,15 +13,19 @@ class MainController extends AbstractController
 {
     public function __construct(
         private ServiceService $service,
-        private ServiceItemsService $itemsService
+        private ServiceItemsService $itemsService,
+        private GalleryService $galleryService
     )
     {
     }
 
-    #[Route('/')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
-        return $this->render('@Twig/main/index.html.twig');
+        return $this->render('@Twig/main/index.html.twig', [
+            'items' => $this->itemsService->getAndGroupItems(),
+            'images' => $this->galleryService->getImages(6)
+        ]);
     }
 
     #[Route ('/about', name: 'about')]
@@ -40,6 +45,20 @@ class MainController extends AbstractController
     {
         return $this->render('@Twig/main/price.html.twig', [
             'items' => $this->itemsService->getAndGroupItems()
+        ]);
+    }
+
+    #[Route ('/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('@Twig/main/contact.html.twig');
+    }
+
+    #[Route ('/gallery', name: 'gallery')]
+    public function gallery(): Response
+    {
+        return $this->render('@Twig/main/gallery.html.twig', [
+            'images' => $this->galleryService->getImages()
         ]);
     }
 }
